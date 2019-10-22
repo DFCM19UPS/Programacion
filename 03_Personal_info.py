86153d1e677b60659ff8ffb95d23a2a9e2c31704
DFCM19UPS <56598629+DFCM19UPS@users.noreply.github.com>
2019-10-22 09:57:54 -0500

    Add files via upload

---
+++ 03_Personal_info.py
@@ -0,0 +1,6 @@
+firstname = input("What is your first name?")
+lastname = input ("What is your las name?")
+location = input ("What is your location?")
+age = input ("How old are you?")
+space= ""
+print ("Hello",space,firstname,space,lastname,space,"your location is",space,location,"and your age is",space,age)
